19e0f696c8c4d285b27db6233e9e5f128b8090db
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-03-31 23:02:43 +0100

    patched token

--- contracts/VolcanoCoin.sol
+++ contracts/VolcanoCoin.sol
@@ -4,8 +4,22 @@ pragma solidity ^0.8.0;
 contract VolcanoCoin {
     uint256 public totalSupply = 10000 ether;
     address owner;
+    mapping(address => uint) balances;
 
-    constructor() {}
+    struct Payment {
+        uint256 amount;
+        address to;
+    }
+
+    mapping(address => Payment) payments;
+
+    event increaseTotalSupply(uint256 amount);
+    event transfer(address recipient, uint256 amount);
+
+    constructor() {
+        owner = msg.sender;
+        balances[msg.sender] = totalSupply;
+    }
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -18,5 +32,17 @@ contract VolcanoCoin {
 
     function increasTotalSupply() public onlyOwner {
         totalSupply += 1000 ether;
+        emit increaseTotalSupply(totalSupply);
+    }
+
+    function getBalanceOf(address account) public returns (uint256) {
+        return balances[account];
+    }
+
+    function transferToken(address receiver, uint256 amount) public {
+        require(balances[msg.sender] >= amount);
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit transfer(receiver, amount);
     }
 }
